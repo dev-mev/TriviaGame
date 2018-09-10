8e5b823c6dfd61a85014c19541762513e7c417a6
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-09-09 21:30:52 -0700

    got basic game functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,213 @@
 //VARIABLES
-var showQuestions = false
-var timer
+var showQuestions = false;
+var timer;
+var userGuess;
+var correct;
+var wrong;
+var unanswered;
+var interval;
+
+var questionsAnswers = [
+    {
+        question: "What's Roxy's middle name?",
+        answers: [
+            {name: "Paris Hilton", isCorrect: false},
+            {name: "Ivanka Trump", isCorrect: false},
+            {name: "Joan Jett", isCorrect: true}
+        ]
+    },
+    {
+        question: "What's Roxy's favorite time of day?",
+        answers: [
+            {name: "Ear cleaning time", isCorrect: false},
+            {name: "Breakfast time", isCorrect: true},
+            {name: "Toothbrushing time", isCorrect: false}
+        ]
+    },
+    {
+        question: "Who is Roxy's favorite supreme court justice?",
+        answers: [
+            {name: "Neil Gorsuch", isCorrect: false},
+            {name: "Ruth Bader Ginsburg", isCorrect: true},
+            {name: "Clarence Thomas", isCorrect: false}
+        ]
+    },
+    {
+        question: "What is Roxy's favorite song?",
+        answers: [
+            {name: "'Roxanne' by the Police", isCorrect: true},
+            {name: "'Every Breath You Take' by the Police", isCorrect: false},
+            {name: "'Don't Stand So Close to Me' by the Police", isCorrect: false}
+        ]
+    }
+]
 
 //FUNCTIONS
-function hideButton(){
-    if(showQuestions){
-        $("#questionScreen").show();
-        $("#startButton").hide();
-    }
-}  
-
 //start button functionality - starts timer counting down
 function startGame(){
-    timer = 100
-    $("#questionScreen").hide();
+    timer = 120
+    correct = 0
+    wrong = 0
+    unanswered = 0
+    $("#startButton").show();
+    $("#resultsCard").hide();
+    $("#badJob").hide();
+    $("#goodJob").hide();
+    $("#questionsCard").hide();
     $("#startButton").click(function(){
         showQuestions = true;
-        hideButton();
+        $("#startButton").hide();
+        $("#questionsCard").show();
         $("#timer").text(timer);
-        setInterval(countDown, 1000);
+        interval = setInterval(countDown, 1000);
+        listQuestions();
     })
 }
 
+//timer counts down
 function countDown(){
-    $("#timer").text(--timer);
+    if(timer >= 1){
+        $("#timer").text(--timer);
+        }
+    else{
+        gradeAnswers();
+    }
 }
 
+//writes the question and answer to the element questionScreen
+function listQuestions(){
+    $("#questionScreen").empty();
+    for (var questionInfo of questionsAnswers) {
+        $("#questionScreen").append(createSingleQuestion(questionInfo));
+    }
+}
 
+//returns question from questionInfo and creates element for answers to go into
+function createSingleQuestion(question){
+    var questionContainer = $("<div>");
+    var questionPrompt = $("<p>").text(question.question);
+    var answers = $("<div class=\"btn-group btn-group-toggle\" data-toggle=\"buttons\">");
 
-//timer
-// timeConverter: function(t) {
+    for (var answerInfo of question.answers) {
+        answers.append(createSingleButton(question, answerInfo));
+    }
 
-//     var minutes = Math.floor(t / 60);
-//     var seconds = t - (minutes * 60);
+    questionContainer.append(questionPrompt, answers);
 
-//     if (seconds < 10) {
-//       seconds = "0" + seconds;
-//     }
+    return questionContainer;
+}
 
-//     if (minutes === 0) {
-//       minutes = "00";
-//     }
-//     else if (minutes < 10) {
-//       minutes = "0" + minutes;
-//     }
+//returns the name property of the answer array in questionsAnswers - sets userGuess value
+function createSingleButton(question, answer) {
+    return $("<label class=\"btn btn-secondary\">")
+        .click(function(){
+            question.hasCorrectAnswer = answer.isCorrect;
+        })
+        .text(answer.name)
+        .append(
+            $("<input type=\"radio\" name=\"options\" autocomplete=\"off\">")
+        );
+}
 
-//     return minutes + ":" + seconds;
-//   }
-// };
+$("#submit").click(function(){
+gradeAnswers();
+})
 
+function gradeAnswers(){
+    for(var questionInfo of questionsAnswers){
+        if(questionInfo.hasCorrectAnswer === true){
+            correct++;
+        }
+        else if(questionInfo.hasCorrectAnswer === false){
+            wrong++;
+        }
+        else{
+            unanswered++;
+        }
+    }
+    clearInterval(interval);
+    resultsScreen();
+}
+
+function resultsScreen(){
+    $("#questionsCard").hide();
+    $("#resultsCard").show();
+    $("#results").prepend("<h1>" + "RESULTS");
+    $("#results").append("<p>" + "Answers correct: " + correct);
+    $("#results").append("<p>" + "Answers wrong: " + wrong);
+    $("#results").append("<p>" + "Unanswered: " + unanswered);
+    if(correct <= 2){
+        $("#badJob").show();
+    }
+    else{
+        $("#goodJob").show();
+    }
+}
+
+$("#reset").click(function(){
+    startGame();
+    })
 //EVENTS
 startGame();
 
 
-//store questions and answers
+
 
 //log answer choices
 
 //endScreen displays number correct, incorrect, and unanswered
+
+
+// for (var questionInfo of questionsAnswers) {
+//     console.log(questionInfo.question);
+
+//     for (var answerInfo of questionInfo.answers) {
+//         console.log("> " + answerInfo.name);
+//     }
+// }
+
+
+
+
+// $("body").append(
+// 	$("<div>")
+// 		.addClass("btn-group btn-group-toggle")
+// 		.attr("data-toggle", "buttons")
+// 		.append(
+// 			$("<label>")
+// 				.addClass("btn btn-secondary")
+//                 .click(function () { console.log("you clicked me!"); })
+//                 .text(questionsAnswers[i].answers[a].name)
+// 				.append(
+// 					$("<input>")
+// 						.attr("type", "radio")
+//                         .attr("name", "options")
+//                         .attr("id", "option1")
+// 						.attr("autocomplete", "off")
+// 				)
+// 		)
+// 		.append(
+// 			$("<label>")
+// 				.addClass("btn btn-secondary")
+//                 .click(function () { console.log("you clicked me!"); })
+//                 .text("Radio button 2")
+// 				.append(
+// 					$("<input>")
+// 						.attr("type", "radio")
+// 						.attr("name", "options")
+// 						.attr("autocomplete", "off")
+// 				)
+// 		)
+// 		.append(
+// 			$("<label>")
+// 				.addClass("btn btn-secondary")
+//                 .click(function () { console.log("you clicked me!"); })
+//                 .text("Radio button 3")
+// 				.append(
+// 					$("<input>")
+// 						.attr("type", "radio")
+// 						.attr("name", "options")
+// 						.attr("autocomplete", "off")
+// 				)
+// 		)
+//     );
